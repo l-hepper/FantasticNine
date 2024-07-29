c15ad9d5b371d8159341d2865325b727644988e6
Patrik Turcani <pturcani@spartaglobal.com>
2024-07-29 16:23:11 +0100

    Implemented a sample connection to the database

--- FantasticNineWebAndAPI/src/test/java/com/sparta/doom/fantasticninewebandapi/FantasticNineWebAndApiApplicationTests.java
+++ FantasticNineWebAndAPI/src/test/java/com/sparta/doom/fantasticninewebandapi/FantasticNineWebAndApiApplicationTests.java
@@ -1,13 +1,28 @@
 package com.sparta.doom.fantasticninewebandapi;
 
+import com.sparta.doom.fantasticninewebandapi.models.MoviesModel;
+import com.sparta.doom.fantasticninewebandapi.repositories.MoviesRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootTest
-class FantasticNineWebAndApiApplicationTests {
+@EnableMongoRepositories
+class MongoDbTestApplicationTests {
+
+	@Autowired
+	private MoviesRepository moviesRepository;
 
 	@Test
 	void contextLoads() {
+
+		System.out.println("_______");
+		for (MoviesModel movie : moviesRepository.findAll()) {
+			System.out.println("Movie: " + movie);
+
+		}
+
 	}
 
 }
