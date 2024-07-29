21205ef5d7d10373ee8b63e74e26d1995d92b926
Katherine <katabeth.ch@gmail.com>
2024-07-29 13:32:23 +0100

    Created the inital project called FantasticNineWebAndAPI

---
+++ FantasticNineWebAndAPI/src/main/java/com/sparta/fnine/fantasticninewebandapi/FantasticNineWebAndApiApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.kch.fantasticninewebandapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasticNineWebAndApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasticNineWebAndApiApplication.class, args);
+	}
+
+}
---
+++ FantasticNineWebAndAPI/src/test/java/com/sparta/kch/fantasticninewebandapi/FantasticNineWebAndApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.kch.fantasticninewebandapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasticNineWebAndApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
