81cfbe7e87ace7eae6ea78128f83cb937699c907
Chris Rayner <crayner@spartaglobal.com>
2024-07-29 16:38:49 +0100

    init and setup Comments and CommentsService

---
+++ FantasticNineWebAndAPI/src/main/java/com/sparta/doom/fantasticninewebandapi/models/Comments.java
@@ -0,0 +1,68 @@
+package com.sparta.doom.fantasticninewebandapi.models;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDate;
+import java.util.Date;
+
+
+@Document(collation = "comments")
+public class Comments {
+
+    @Id
+    private ObjectId id;
+    private String email;
+    private Date date;
+    private String text;
+    private String name;
+    private ObjectId movie_id;
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ObjectId getMovie_id() {
+        return movie_id;
+    }
+
+    public void setMovie_id(ObjectId movie_id) {
+        this.movie_id = movie_id;
+    }
+}
---
+++ FantasticNineWebAndAPI/src/test/java/com/sparta/doom/fantasticninewebandapi/service/MoviesServiceTests.java
@@ -0,0 +1,4 @@
+package com.sparta.doom.fantasticninewebandapi.service;
+
+public class MoviesServiceTests {
+}
