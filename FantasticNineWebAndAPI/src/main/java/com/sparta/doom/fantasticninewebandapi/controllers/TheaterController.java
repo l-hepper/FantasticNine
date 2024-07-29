d6f4813b56325eb87d678ff635dc218d721bd333
Liam <33605657+l-hepper@users.noreply.github.com>
2024-07-29 17:05:21 +0100

    Added working get method to retrieve all theaters

--- FantasticNineWebAndAPI/src/main/java/com/sparta/doom/fantasticninewebandapi/controllers/TheaterController.java
+++ FantasticNineWebAndAPI/src/main/java/com/sparta/doom/fantasticninewebandapi/controllers/TheaterController.java
@@ -1,14 +1,29 @@
 package com.sparta.doom.fantasticninewebandapi.controllers;
 
+import com.sparta.doom.fantasticninewebandapi.models.TheaterModel;
+import com.sparta.doom.fantasticninewebandapi.repositories.TheaterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TheaterController {
 
-    private final TheaterRepository;
+    private final TheaterRepository theaterRepository;
 
+    @Autowired
     public TheaterController(TheaterRepository theaterRepository) {
-
+        this.theaterRepository = theaterRepository;
     }
+
+    @GetMapping("/theaters")
+    public ResponseEntity<List<TheaterModel>> getTheaters() {
+        List<TheaterModel> theaters = theaterRepository.findAll();
+        return ResponseEntity.ok(theaters);
+    }
+
 
 }
