1f8d36c092b810238fcce76d5fe304bceb040ee0
Katherine <katabeth.ch@gmail.com>
2024-07-29 14:23:48 +0100

    Changed fnine to doom

--- FantasticNineWebAndAPI/src/main/java/com/sparta/doom/fantasticninewebandapi/FantasticNineWebAndApiApplication.java
+++ FantasticNineWebAndAPI/src/main/java/com/sparta/doom/fantasticninewebandapi/FantasticNineWebAndApiApplication.java
@@ -1,4 +1,4 @@
-package com.sparta.fnine.fantasticninewebandapi;
+package com.sparta.doom.fantasticninewebandapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
