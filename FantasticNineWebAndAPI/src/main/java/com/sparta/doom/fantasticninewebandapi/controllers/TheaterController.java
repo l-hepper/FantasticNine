b3ab40a1d557cb25c1ea765fb6a3e88ff1d66641
Liam <33605657+l-hepper@users.noreply.github.com>
2024-07-30 11:04:50 +0100

    TheaterController accesses TheaterService for method calls

--- FantasticNineWebAndAPI/src/main/java/com/sparta/doom/fantasticninewebandapi/controllers/TheaterController.java
+++ FantasticNineWebAndAPI/src/main/java/com/sparta/doom/fantasticninewebandapi/controllers/TheaterController.java
@@ -2,26 +2,29 @@ package com.sparta.doom.fantasticninewebandapi.controllers;
 
 import com.sparta.doom.fantasticninewebandapi.models.TheaterModel;
 import com.sparta.doom.fantasticninewebandapi.repositories.TheaterRepository;
+import com.sparta.doom.fantasticninewebandapi.services.TheaterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class TheaterController {
 
-    private final TheaterRepository theaterRepository;
+    private final TheaterService theaterService;
 
     @Autowired
-    public TheaterController(TheaterRepository theaterRepository) {
-        this.theaterRepository = theaterRepository;
+    public TheaterController(TheaterService theaterService) {
+        this.theaterService = theaterService;
     }
 
     @GetMapping("/theaters")
     public ResponseEntity<List<TheaterModel>> getTheaters() {
-        List<TheaterModel> theaters = theaterRepository.findAll();
+        List<TheaterModel> theaters = theaterService.getAllTheaters();
         return ResponseEntity.ok(theaters);
     }
 
