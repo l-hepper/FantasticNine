04f53d1a7f58b7cf84a2da8f23691b536edc0676
Katherine <katabeth.ch@gmail.com>
2024-07-30 16:56:06 +0100

    Still fixing it

--- FantasticNineWebAndAPI/src/main/java/com/sparta/doom/fantasticninewebandapi/controllers/web/ScheduleMvcController.java
+++ FantasticNineWebAndAPI/src/main/java/com/sparta/doom/fantasticninewebandapi/controllers/web/ScheduleMvcController.java
@@ -1,5 +1,6 @@
 package com.sparta.doom.fantasticninewebandapi.controllers.web;
 
+import com.sparta.doom.fantasticninewebandapi.repositories.ScheduleRepository;
 import com.sparta.doom.fantasticninewebandapi.services.ScheduleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,22 +16,30 @@ public class ScheduleMvcController {
     // TODO Theatre has one schedule with many showings
     // TODO movies have many schedules with many showings at many theatres
     // TODO searches are based off of what called it
-    private final ScheduleService ScheduleService;
+    private final ScheduleService scheduleService;
+    private ScheduleRepository scheduleRepository;
     @Autowired
-    public ScheduleMvcController(ScheduleService scheduleService) {
-        ScheduleService = scheduleService;
+    public ScheduleMvcController(ScheduleService scheduleService, ScheduleRepository scheduleRepository) {
+        this.scheduleService = scheduleService;
+        this.scheduleRepository = scheduleRepository;
     }
     @GetMapping("/theatre/search/{id}/schedules/")
     public String getScheduleForTheatre(@PathVariable String id, Model model) {
-        model.addAttribute("schedules", ScheduleService.getSchedulesByTheatreId(id));
+        model.addAttribute("schedules", scheduleService.getSchedulesByTheatreId(id));
         model.addAttribute("searchType", "theatre");
         return "schedules";
     }
 
     @GetMapping("/movies/search/{id}/schedules/")
     public String getScheduleForMovie(@PathVariable String id, Model model) {
-        model.addAttribute("schedules", ScheduleService.getSchedulesByMovieId(id));
+        model.addAttribute("schedules", scheduleService.getSchedulesByMovieId(id));
         model.addAttribute("searchType", "movie");
+        return "schedules";
+    }
+    @GetMapping("/schedules/")
+    public String getAllSchedules(Model model) {
+        model.addAttribute("schedules", scheduleRepository.findAll());
+        model.addAttribute("searchType", "all");
         return "schedules";
     }
 
