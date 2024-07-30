dc396547a4deae6f95f06741495af2821c00fc42
Patrik Turcani <pturcani@spartaglobal.com>
2024-07-30 22:27:32 +0100

    Added basic pagination to getAllMovies and made service method to retrieve series from the DB

--- FantasticNineWebAndAPI/src/main/java/com/sparta/doom/fantasticninewebandapi/services/MoviesService.java
+++ FantasticNineWebAndAPI/src/main/java/com/sparta/doom/fantasticninewebandapi/services/MoviesService.java
@@ -4,6 +4,9 @@ import com.sparta.doom.fantasticninewebandapi.dtos.MoviesDTO;
 import com.sparta.doom.fantasticninewebandapi.models.MovieDoc;
 import com.sparta.doom.fantasticninewebandapi.repositories.MoviesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -29,56 +32,12 @@ public class MoviesService {
         this.mongoTemplate = mongoTemplate;
     }
 
-    public MoviesDTO convertToDto(MovieDoc movieDoc) {
-        return new MoviesDTO(
-                movieDoc.getId(),
-                movieDoc.getAwards(),
-                movieDoc.getCast(),
-                movieDoc.getCountries(),
-                movieDoc.getDirectors(),
-                movieDoc.getFullplot(),
-                movieDoc.getGenres(),
-                movieDoc.getImdb(),
-                movieDoc.getLanguages(),
-                movieDoc.getLastupdated(),
-                movieDoc.getNum_mflix_comments(),
-                movieDoc.getPlot(),
-                movieDoc.getPoster(),
-                movieDoc.getRated(),
-                movieDoc.getReleased(),
-                movieDoc.getRuntime(),
-                movieDoc.getTitle(),
-                movieDoc.getTomatoes(),
-                movieDoc.getType(),
-                movieDoc.getWriters(),
-                movieDoc.getYear()
-        );
-    }
 
-    private MovieDoc convertToEntity(MoviesDTO movieDto) {
-        MovieDoc movieDoc = new MovieDoc();
-        movieDoc.setId(movieDto.getId());
-        movieDoc.setAwards(movieDto.getAwards());
-        movieDoc.setCast(movieDto.getCast());
-        movieDoc.setCountries(movieDto.getCountries());
-        movieDoc.setDirectors(movieDto.getDirectors());
-        movieDoc.setFullplot(movieDto.getFullplot());
-        movieDoc.setGenres(movieDto.getGenres());
-        movieDoc.setImdb(movieDto.getImdb());
-        movieDoc.setLanguages(movieDto.getLanguages());
-        movieDoc.setLastupdated(movieDto.getLastupdated());
-        movieDoc.setNum_mflix_comments(movieDto.getNumMflixComments());
-        movieDoc.setPlot(movieDto.getPlot());
-        movieDoc.setPoster(movieDto.getPoster());
-        movieDoc.setRated(movieDto.getRated());
-        movieDoc.setReleased(movieDto.getReleased());
-        movieDoc.setRuntime(movieDto.getRuntime());
-        movieDoc.setTitle(movieDto.getTitle());
-        movieDoc.setTomatoes(movieDto.getTomatoes());
-        movieDoc.setType(movieDto.getType());
-        movieDoc.setWriters(movieDto.getWriters());
-        movieDoc.setYear(movieDto.getYear());
-        return movieDoc;
+
+    public Page<MoviesDTO> getAllMovies(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<MovieDoc> movieDocsPage = moviesRepository.findAll(pageable);
+        return movieDocsPage.map(this::convertToDto);
     }
 
     public List<MoviesDTO> getAllMovies() {
@@ -97,6 +56,15 @@ public class MoviesService {
         return moviesRepository.findAll().stream()
                 .filter(movieDoc -> movieDoc.getTitle().contains(title))
                 .findFirst();
+    }
+
+    public List<MoviesDTO> getAllSeries() {
+        List<MovieDoc> movieDocs = moviesRepository.findAll().stream()
+                .filter(movieDoc -> "series".equalsIgnoreCase(movieDoc.getType()))
+                .toList();
+        return movieDocs.stream()
+                .map(this::convertToDto)
+                .collect(Collectors.toList());
     }
 
     public List<MovieDoc> getMoviesByPartialTitle(String title) {
@@ -153,5 +121,57 @@ public class MoviesService {
         return topMovieDocs.stream()
                 .map(this::convertToDto)
                 .collect(Collectors.toList());
+    }
+
+    public MoviesDTO convertToDto(MovieDoc movieDoc) {
+        return new MoviesDTO(
+                movieDoc.getId(),
+                movieDoc.getAwards(),
+                movieDoc.getCast(),
+                movieDoc.getCountries(),
+                movieDoc.getDirectors(),
+                movieDoc.getFullplot(),
+                movieDoc.getGenres(),
+                movieDoc.getImdb(),
+                movieDoc.getLanguages(),
+                movieDoc.getLastupdated(),
+                movieDoc.getNum_mflix_comments(),
+                movieDoc.getPlot(),
+                movieDoc.getPoster(),
+                movieDoc.getRated(),
+                movieDoc.getReleased(),
+                movieDoc.getRuntime(),
+                movieDoc.getTitle(),
+                movieDoc.getTomatoes(),
+                movieDoc.getType(),
+                movieDoc.getWriters(),
+                movieDoc.getYear()
+        );
+    }
+
+    private MovieDoc convertToEntity(MoviesDTO movieDto) {
+        MovieDoc movieDoc = new MovieDoc();
+        movieDoc.setId(movieDto.getId());
+        movieDoc.setAwards(movieDto.getAwards());
+        movieDoc.setCast(movieDto.getCast());
+        movieDoc.setCountries(movieDto.getCountries());
+        movieDoc.setDirectors(movieDto.getDirectors());
+        movieDoc.setFullplot(movieDto.getFullplot());
+        movieDoc.setGenres(movieDto.getGenres());
+        movieDoc.setImdb(movieDto.getImdb());
+        movieDoc.setLanguages(movieDto.getLanguages());
+        movieDoc.setLastupdated(movieDto.getLastupdated());
+        movieDoc.setNum_mflix_comments(movieDto.getNumMflixComments());
+        movieDoc.setPlot(movieDto.getPlot());
+        movieDoc.setPoster(movieDto.getPoster());
+        movieDoc.setRated(movieDto.getRated());
+        movieDoc.setReleased(movieDto.getReleased());
+        movieDoc.setRuntime(movieDto.getRuntime());
+        movieDoc.setTitle(movieDto.getTitle());
+        movieDoc.setTomatoes(movieDto.getTomatoes());
+        movieDoc.setType(movieDto.getType());
+        movieDoc.setWriters(movieDto.getWriters());
+        movieDoc.setYear(movieDto.getYear());
+        return movieDoc;
     }
 }
