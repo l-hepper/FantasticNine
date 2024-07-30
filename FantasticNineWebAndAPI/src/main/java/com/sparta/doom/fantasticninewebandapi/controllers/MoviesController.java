7b74a7863a4411a9a730d94968324b431699e9d5
Patrik Turcani <pturcani@spartaglobal.com>
2024-07-30 10:25:06 +0100

    added controller methods and fixed services

--- FantasticNineWebAndAPI/src/main/java/com/sparta/doom/fantasticninewebandapi/controllers/MoviesController.java
+++ FantasticNineWebAndAPI/src/main/java/com/sparta/doom/fantasticninewebandapi/controllers/MoviesController.java
@@ -1,29 +1,63 @@
 package com.sparta.doom.fantasticninewebandapi.controllers;
 
+import com.sparta.doom.fantasticninewebandapi.dtos.MoviesDTO;
 import com.sparta.doom.fantasticninewebandapi.models.MoviesModel;
-import com.sparta.doom.fantasticninewebandapi.models.TheaterModel;
-import com.sparta.doom.fantasticninewebandapi.repositories.MoviesRepository;
+import com.sparta.doom.fantasticninewebandapi.services.MoviesService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/movies")
 public class MoviesController {
 
-    private MoviesRepository moviesRepository;
+    private final MoviesService moviesService;
 
-    public MoviesController(MoviesRepository moviesRepository) {
-        this.moviesRepository = moviesRepository;
+    public MoviesController(MoviesService moviesService) {
+        this.moviesService = moviesService;
     }
 
     @GetMapping
-    public ResponseEntity<List<MoviesModel>> getTheaters() {
-        List<MoviesModel> movies = moviesRepository.findAll();
+    public ResponseEntity<List<MoviesDTO>> getAllMovies() {
+        List<MoviesDTO> movies = moviesService.getAllMovies();
         return ResponseEntity.ok(movies);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<MoviesDTO> getMovieById(@PathVariable String id) {
+        MoviesDTO movie = moviesService.getMovieById(id);
+        return movie != null ? ResponseEntity.ok(movie) : ResponseEntity.notFound().build();
+    }
+
+    //TODO add regex
+    @GetMapping("/title/{title}")
+    public ResponseEntity<MoviesDTO> getMovieByTitle(@PathVariable String title) {
+        Optional<MoviesModel> movie = moviesService.getMovieByTitle(title);
+        if (movie.isPresent()) {
+            MoviesDTO movieDto = moviesService.convertToDto(movie.get());
+            return ResponseEntity.ok(movieDto);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<MoviesDTO> createMovie(@RequestBody MoviesDTO movieDto) {
+        MoviesDTO createdMovie = moviesService.createMovie(movieDto);
+        return ResponseEntity.status(201).body(createdMovie);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MoviesDTO> updateMovie(@PathVariable String id, @RequestBody MoviesDTO movieDto) {
+        MoviesDTO updatedMovie = moviesService.updateMovie(id, movieDto);
+        return updatedMovie != null ? ResponseEntity.ok(updatedMovie) : ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMovie(@PathVariable String id) {
+        boolean isDeleted = moviesService.deleteMovie(id);
+        return isDeleted ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
+    }
 }
--- FantasticNineWebAndAPI/src/main/java/com/sparta/doom/fantasticninewebandapi/services/MoviesService.java
+++ FantasticNineWebAndAPI/src/main/java/com/sparta/doom/fantasticninewebandapi/services/MoviesService.java
@@ -10,6 +10,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+
+//TODO Add Pagination
 @Service
 public class MoviesService {
 
