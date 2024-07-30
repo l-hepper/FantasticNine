1b93a6cd9fab3109d0df52f6c2c9e4a93b643c92
Liam <33605657+l-hepper@users.noreply.github.com>
2024-07-30 18:30:36 +0100

    deleteTheater end point added

--- FantasticNineWebAndAPI/src/main/java/com/sparta/doom/fantasticninewebandapi/controllers/api/TheaterApiController.java
+++ FantasticNineWebAndAPI/src/main/java/com/sparta/doom/fantasticninewebandapi/controllers/api/TheaterApiController.java
@@ -3,12 +3,10 @@ package com.sparta.doom.fantasticninewebandapi.controllers.api;
 import com.sparta.doom.fantasticninewebandapi.models.theater.TheaterDoc;
 import com.sparta.doom.fantasticninewebandapi.services.TheaterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,5 +35,11 @@ public class TheaterApiController {
         return ResponseEntity.ok()
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(theater);
+    }
+
+    @DeleteMapping("theaters/{id}")
+    public ResponseEntity<HttpStatus> deleteTheater(int id) {
+        theaterService.deleteTheaterByTheaterId(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
