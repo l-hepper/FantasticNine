f7fd7907f99107e445879038de61cfc433dd546f
Chris Rayner <crayner@spartaglobal.com>
2024-07-30 11:01:03 +0100

    added secure endpoints to CommentsApiController

--- FantasticNineWebAndAPI/src/main/java/com/sparta/doom/fantasticninewebandapi/controllers/CommentsApiController.java
+++ FantasticNineWebAndAPI/src/main/java/com/sparta/doom/fantasticninewebandapi/controllers/CommentsApiController.java
@@ -2,6 +2,7 @@ package com.sparta.doom.fantasticninewebandapi.controllers;
 
 import com.sparta.doom.fantasticninewebandapi.models.CommentDoc;
 import com.sparta.doom.fantasticninewebandapi.services.CommentsService;
+import com.sparta.doom.fantasticninewebandapi.services.SecurityService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
@@ -11,20 +12,24 @@ import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.net.URI;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/")
 public class CommentsApiController {
 
     private final CommentsService commentsService;
+    private final SecurityService securityService;
 
     @Autowired
-    public CommentsApiController(CommentsService commentsService) {
+    public CommentsApiController(CommentsService commentsService, SecurityService securityService) {
         this.commentsService = commentsService;
+        this.securityService = securityService;
     }
 
     @GetMapping("/movies/{movie}/comments")
@@ -73,8 +78,15 @@ public class CommentsApiController {
         comments = comments.stream().filter(c -> commentsService.getCommentsByDateRange(date1, date2).contains(c)).toList();
         return new ResponseEntity<>(CollectionModel.of(comments), HttpStatus.OK);
     }
+
     @PostMapping("/{movie}/comments/create")
-    public ResponseEntity<EntityModel<CommentDoc>> createComment(@PathVariable("movie") ObjectId movieId, @RequestBody CommentDoc newComment) {
+    public ResponseEntity<EntityModel<CommentDoc>> createComment(@RequestHeader(name = "DOOM-API-KEY") String key,@PathVariable("movie") ObjectId movieId, @RequestBody CommentDoc newComment) {
+        Optional<String> requestRole = securityService.getRoleFromKey(key);
+        if (requestRole.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No key found");
+        } else if (!requestRole.get().equals("FULL_ACCESS")) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Unauthorized");
+        }
         if(!newComment.getId().equals(movieId)){
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
@@ -84,7 +96,13 @@ public class CommentsApiController {
         return ResponseEntity.created(location).body(EntityModel.of(newComment).add(selfLink));
     }
     @PutMapping("/{movie}/comments/{commentId}")
-    public ResponseEntity<CommentDoc> updateComment(@PathVariable("movie") ObjectId movieId, @PathVariable("commentId") ObjectId commentId, @RequestBody CommentDoc newComment) {
+    public ResponseEntity<CommentDoc> updateComment(@RequestHeader(name = "DOOM-API-KEY") String key,@PathVariable("movie") ObjectId movieId, @PathVariable("commentId") ObjectId commentId, @RequestBody CommentDoc newComment) {
+        Optional<String> requestRole = securityService.getRoleFromKey(key);
+        if (requestRole.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No key found");
+        } else if (!requestRole.get().equals("FULL_ACCESS")) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Unauthorized");
+        }
         CommentDoc oldComment = commentsService.getCommentById(commentId);
         if(oldComment == null){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -96,7 +114,13 @@ public class CommentsApiController {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
     @DeleteMapping("/{movie}/comments/{commentId}")
-    public ResponseEntity<CommentDoc> deleteComment(@PathVariable("commentId") ObjectId commentId) {
+    public ResponseEntity<CommentDoc> deleteComment(@RequestHeader(name = "DOOM-API-KEY") String key, @PathVariable("commentId") ObjectId commentId) {
+        Optional<String> requestRole = securityService.getRoleFromKey(key);
+        if (requestRole.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No key found");
+        } else if (!requestRole.get().equals("FULL_ACCESS")) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Unauthorized");
+        }
         CommentDoc comment = commentsService.getCommentById(commentId);
         if(comment == null){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
