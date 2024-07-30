048db7f2cdf1e4016142f531e05dc9a444681ca6
Gilbert Reid <greid@spartaglobal.com>
2024-07-30 11:59:50 +0100

    created schedule service with tests

--- FantasticNineWebAndAPI/src/main/java/com/sparta/doom/fantasticninewebandapi/models/Theatre.java
+++ FantasticNineWebAndAPI/src/main/java/com/sparta/doom/fantasticninewebandapi/models/Theatre.java
@@ -1,11 +1,13 @@
 package com.sparta.doom.fantasticninewebandapi.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 
 @Data
 @Setter
+@AllArgsConstructor
 public class Theatre {
     @Id
     private String id;
--- FantasticNineWebAndAPI/src/test/java/com/sparta/doom/fantasticninewebandapi/ScheduleRepoTest.java
+++ FantasticNineWebAndAPI/src/test/java/com/sparta/doom/fantasticninewebandapi/ScheduleRepoTest.java
@@ -4,11 +4,13 @@ import com.sparta.doom.fantasticninewebandapi.models.Movie;
 import com.sparta.doom.fantasticninewebandapi.models.Schedule;
 import com.sparta.doom.fantasticninewebandapi.models.Theatre;
 import com.sparta.doom.fantasticninewebandapi.repositories.ScheduleRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootTest
 public class ScheduleRepoTest {
@@ -19,26 +21,20 @@ public class ScheduleRepoTest {
     ScheduleRepository scheduleRepository;
     @Test
     void canAddSchedule(){
-
+        long count = scheduleRepository.count();
+        Theatre theatre = new Theatre("59a47286cfa9a3a73e51e732");
+        Movie movie = new Movie("573a1390f29313caabcd42e8");
         Schedule schedule = new Schedule();
-        Movie movie = new Movie();
-        movie.setId("dddd");
-        schedule.setMovie(movie);
-        Theatre theatre = new Theatre();
-        theatre.setId("rhguireghdrg");
         schedule.setTheatre(theatre);
+        schedule.setMovie(movie);
         schedule.setStartTime(LocalDateTime.now());
         scheduleRepository.save(schedule);
-        System.out.println(scheduleRepository.findAll());
-
+        Assertions.assertEquals(count +1, scheduleRepository.count());
     }
 
     @Test
     void canRemoveSchedule(){
-        Schedule schedule = new Schedule();
-        schedule.setId("66a8b0d4506f5a1a45ef7d64");
-        scheduleRepository.delete(schedule);
-        System.out.println(scheduleRepository.findAll());
+        //TODO: Create a test for deleting it'd have to mock or something somehow
     }
 
 }
