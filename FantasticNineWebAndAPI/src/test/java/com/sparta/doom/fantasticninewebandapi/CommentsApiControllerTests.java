d91b7e67509315819dae8366c4c7824e09ec2ff0
Chris Rayner <crayner@spartaglobal.com>
2024-07-30 21:11:20 +0100

    finished tests for CommentsApiController

--- FantasticNineWebAndAPI/src/test/java/com/sparta/doom/fantasticninewebandapi/CommentsApiControllerTests.java
+++ FantasticNineWebAndAPI/src/test/java/com/sparta/doom/fantasticninewebandapi/CommentsApiControllerTests.java
@@ -2,8 +2,6 @@ package com.sparta.doom.fantasticninewebandapi;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sparta.doom.fantasticninewebandapi.models.CommentDoc;
-import com.sparta.doom.fantasticninewebandapi.services.CommentsService;
-import com.sparta.doom.fantasticninewebandapi.services.SecurityService;
 import org.bson.types.ObjectId;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,7 +9,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.web.servlet.MockMvc;
@@ -139,7 +136,7 @@ public class CommentsApiControllerTests {
     }
 
     @Test
-    void testCreateNewCommentReturnsNoContentIfCorrect() throws Exception {
+    void testCreateNewCommentReturnsIsCreatedIfCorrect() throws Exception {
         CommentDoc commentDoc = new CommentDoc();
         ObjectId movieId = new ObjectId("573a1390f29313caabcd4323");
         commentDoc.setMovie_id(movieId);
@@ -149,7 +146,6 @@ public class CommentsApiControllerTests {
         commentDoc.setName("Test User");
 
         String commentJson = objectMapper.writeValueAsString(commentDoc);
-        System.out.println(commentJson);
 
         mockMvc.perform(MockMvcRequestBuilders.post("/api/movies/573a1390f29313caabcd4323/comments/create")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -158,47 +154,88 @@ public class CommentsApiControllerTests {
                         .content(commentJson))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isCreated());
-
     }
 
-//    @Test
-//    void testCreateCommentWithNoKey() throws Exception {
-//        mockMvc.perform(MockMvcRequestBuilders.post("/api/movies/573a1390f29313caabcd4323/comments/create")
-//                .header("DOOM-API-KEY", "read-only-key"))
-//                .andExpect(MockMvcResultMatchers.status().isUnauthorized());
-//    }
+    //todo when updating comments need validation to ensure only admin or user can update comment
 
-//    @Test
-//    void testCreateCommentSuccess() throws Exception {
-//        ObjectId movieId = new ObjectId("573a1390f29313caabcd4323");
-//        ObjectId commentId = new ObjectId();
-//        CommentDoc commentDoc = new CommentDoc();
-//        commentDoc.setId(commentId);
-//        commentDoc.setMovie_id(movieId);
-//
-//        mockMvc.perform(MockMvcRequestBuilders.post("/api/movies/573a1390f29313caabcd4323/comments/create")
-//                        .header("DOOM-API-KEY", "FULL_ACCESS")
-//                .content(objectMapper.writeValueAsString(commentDoc)))
-//                .andDo(MockMvcResultHandlers.print());
-//    }
+    @Test
+    void testUpdateCommentReturnsNotFoundIfNoOldComment() throws Exception {
+        CommentDoc commentDoc = new CommentDoc();
+        ObjectId commentId = new ObjectId("5a9427648b0beebeb69579e8");
+        ObjectId movieId = new ObjectId("573a1390f29313caabcd4323");
+        commentDoc.setMovie_id(movieId);
+        commentDoc.setId(commentId);
+        commentDoc.setEmail("test@example.com");
+        commentDoc.setDate(new Date());
+        commentDoc.setText("This is a test comment.");
+        commentDoc.setName("Test User");
+
+        String commentJson = objectMapper.writeValueAsString(commentDoc);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/movies/573a1390f29313caabcd4323/comments/id/5a9427648b0beebeb69579e8")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .header("DOOM-API-KEY", "unique-api-key-123")
+                        .content(commentJson))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    void testUpdateCommentReturnsBadRequestIfWrongMovie() throws Exception {
+        CommentDoc commentDoc = new CommentDoc();
+        ObjectId commentId = new ObjectId("5a9427648b0beebeb69579e7");
+        ObjectId movieId = new ObjectId("573a1390f29313caabcd4323");
+        commentDoc.setMovie_id(movieId);
+        commentDoc.setId(commentId);
+        commentDoc.setEmail("test@example.com");
+        commentDoc.setDate(new Date());
+        commentDoc.setText("This is a test comment.");
+        commentDoc.setName("Test User");
+
+        String commentJson = objectMapper.writeValueAsString(commentDoc);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/movies/573a1390f29313caabcd446f/comments/id/5a9427648b0beebeb69579e7")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .header("DOOM-API-KEY", "unique-api-key-123")
+                        .content(commentJson))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @Test
+    void testUpdateCommentReturnsNoContentIfSuccessful() throws Exception {
+        CommentDoc commentDoc = new CommentDoc();
+        ObjectId commentId = new ObjectId("5a9427648b0beebeb69579e7");
+        ObjectId movieId = new ObjectId("573a1390f29313caabcd4323");
+        commentDoc.setMovie_id(movieId);
+        commentDoc.setId(commentId);
+        commentDoc.setEmail("test@example.com");
+        commentDoc.setDate(new Date());
+        commentDoc.setText("This is a test comment.");
+        commentDoc.setName("Test User");
+
+        String commentJson = objectMapper.writeValueAsString(commentDoc);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/movies/573a1390f29313caabcd4323/comments/id/5a9427648b0beebeb69579e7")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .header("DOOM-API-KEY", "unique-api-key-123")
+                        .content(commentJson))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
 
     @Test
     void testDeleteCommentReturnsCommentNotFound() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.delete("/api/movies/573a1390f29313caabcd4323/comments/id/5a9427648b0beebeb69579e8"))
                 .andExpect(MockMvcResultMatchers.status().isNotFound());
     }
-
-//    @Test
-//    void testDeleteCommentReturnsNoContent() throws Exception {
-//
-////        String apiKey = "valid-api-key";
-////        String role = "FULL_ACCESS";
-////
-////        when(securityService.getRoleFromKey(apiKey)).thenReturn(Optional.of(role));
-////        when(commentsService.getCommentById(commentId)).thenReturn(mockComment);
-//
-//        mockMvc.perform(MockMvcRequestBuilders.delete("/api/movies/573a1390f29313caabcd4323/comments/id/5a9427648b0beebeb69579e7")
-//                        .header("DOOM-API-KEY","unique-api-key-123"))
-//                .andExpect(MockMvcResultMatchers.status().isNoContent());
-//    }
+    @Test
+    void testDeleteCommentReturnsNoContentIfSuccessful() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/movies/573a1390f29313caabcd4323/comments/id/5a9427648b0beebeb69579e7")
+                        .header("DOOM-API-KEY", "unique-api-key-123"))
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
 }
