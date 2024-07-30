c6017d9de876ea8df11ef74061ccde2b348386dd
Liam <33605657+l-hepper@users.noreply.github.com>
2024-07-30 11:57:17 +0100

    Classes added for nested data structures (Address, Geo, Location)

---
+++ FantasticNineWebAndAPI/src/main/java/com/sparta/doom/fantasticninewebandapi/models/Geo.java
@@ -0,0 +1,18 @@
+package com.sparta.doom.fantasticninewebandapi.models;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "geo")
+public class Geo {
+    private String type;
+    private double[] coordinates;
+
+    // Getters and Setters
+    public String getType() { return type; }
+
+    public void setType(String type) { this.type = type; }
+
+    public double[] getCoordinates() { return coordinates; }
+
+    public void setCoordinates(double[] coordinates) { this.coordinates = coordinates; }
+}
---
+++ FantasticNineWebAndAPI/src/main/java/com/sparta/doom/fantasticninewebandapi/models/Location.java
@@ -0,0 +1,18 @@
+package com.sparta.doom.fantasticninewebandapi.models;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "location")
+public class Location {
+    private Address address;
+    private Geo geo;
+
+    // Getters and Setters
+    public Address getAddress() { return address; }
+
+    public void setAddress(Address address) { this.address = address; }
+
+    public Geo getGeo() { return geo; }
+
+    public void setGeo(Geo geo) { this.geo = geo; }
+}
