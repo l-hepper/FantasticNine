0c5bfe1fcf22b2ff53e117dee119ff9a3f3bc0b7
KianDoyleSparta <kdoyle@spartaglobal.com>
2024-07-30 14:34:54 +0100

    Created MoviesWebController class and movies.html template

---
+++ FantasticNineWebAndAPI/src/main/java/com/sparta/doom/fantasticninewebandapi/config/ApiCommsConf.java
@@ -0,0 +1,16 @@
+package com.sparta.doom.fantasticninewebandapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class ApiCommsConf {
+    @Bean
+    public WebClient webClient() {
+        return WebClient
+                .builder()
+                .baseUrl("https://localhost:8080/")
+                .build();
+    }
+}
