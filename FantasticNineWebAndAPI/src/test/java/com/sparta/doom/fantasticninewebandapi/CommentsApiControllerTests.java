f14a4515a5bf9c48eb1014177d8008615c0914e0
Chris Rayner <crayner@spartaglobal.com>
2024-07-31 11:10:20 +0100

    changed CommentsApiControllerTests to pass with new endpoint structure for users

--- FantasticNineWebAndAPI/src/test/java/com/sparta/doom/fantasticninewebandapi/CommentsApiControllerTests.java
+++ FantasticNineWebAndAPI/src/test/java/com/sparta/doom/fantasticninewebandapi/CommentsApiControllerTests.java
@@ -114,7 +114,7 @@ public class CommentsApiControllerTests {
     }
     @Test
     void testGetCommentsByUser() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/users/Mercedes-Tyler/comments"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/users/name/Mercedes-Tyler/comments"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("Mercedes Tyler")))
                 .andDo(MockMvcResultHandlers.print());
@@ -122,7 +122,7 @@ public class CommentsApiControllerTests {
     }
     @Test
     void testGetCommentsByUserNotFound() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/users/MercedesTyler/comments"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/users/name/MercedesTyler/comments"))
                 .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("\"totalElements\":0")));
     }
     @Test
