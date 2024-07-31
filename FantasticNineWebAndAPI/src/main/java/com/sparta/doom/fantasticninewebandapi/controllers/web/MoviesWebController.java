2e3f4d4cefc0377ef13947aa634e6ac38b347606
Lewis McFarlane <lmcfarlane@spartaglobal.com>
2024-07-31 21:39:27 +0100

    feat: build search by movie name in MoviesWebController
    Change url endpoint of getMovieDetails to movies/ignored
    
    Removed deprecated api key assignation

--- FantasticNineWebAndAPI/src/main/java/com/sparta/doom/fantasticninewebandapi/controllers/web/MoviesWebController.java
+++ FantasticNineWebAndAPI/src/main/java/com/sparta/doom/fantasticninewebandapi/controllers/web/MoviesWebController.java
@@ -1,6 +1,7 @@
 package com.sparta.doom.fantasticninewebandapi.controllers.web;
 
 import com.sparta.doom.fantasticninewebandapi.models.MovieDoc;
+import com.sparta.doom.fantasticninewebandapi.models.theater.TheaterDoc;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -18,7 +19,7 @@ public class MoviesWebController {
 
     public final WebClient webClient;
 
-    @Value("${key}")
+//    @Value("${key}")
     private String key;
 
     public MoviesWebController(WebClient webClient) {
@@ -46,27 +47,50 @@ public class MoviesWebController {
         return "movies/movies";
     }
 
-    @GetMapping("/search")
-    public String getSearchedMovies(@RequestParam String movieName, Model model) {
-        List<MovieDoc> movies = webClient
+//    @GetMapping
+//    public String getTheatres(
+//            @RequestParam(defaultValue = "0") int page,
+//            Model model) {
+//        List<TheaterDoc> theaters = webClient
+//                .get()
+//                .uri("/api/theaters")
+//                .header("DOOM-API-KEY", key)
+//                .retrieve()
+//                .bodyToFlux(TheaterDoc.class)
+//                .collectList()
+//                .block();
+//
+//        int start = page * PAGE_SIZE;
+//        int end = Math.min(start + PAGE_SIZE, theaters.size());
+//
+//        List<TheaterDoc> paginatedTheaters = theaters.subList(start, end);
+//
+//        model.addAttribute("theaters", paginatedTheaters);
+//        model.addAttribute("currentPage", page);
+//        model.addAttribute("totalPages", (int) Math.ceil((double) theaters.size() / PAGE_SIZE));
+//        return "theaters/theaters";
+//    }
+
+    @GetMapping("/search/{movieName}")
+    public String getSearchedMovies(@PathVariable String movieName, Model model) {
+        ResponseEntity<List<MovieDoc>> movies = webClient
                 .get()
-                .uri("api/movies")
+                .uri("/api/movies/search/" + movieName)
                 .header("DOOM-API-KEY", key)
                 .retrieve()
-                .bodyToFlux(MovieDoc.class)
-                .collectList()
+                .toEntityList(MovieDoc.class)
                 .block();
-        ArrayList<MovieDoc> returnMovies = new ArrayList<>();
-        for (MovieDoc movie : movies) {
-            if (movie.getTitle().toLowerCase().contains(movieName.toLowerCase())) {
-                returnMovies.add(movie);
+        ArrayList<MovieDoc> moviesList = new ArrayList<>();
+        if (movies.hasBody()) {
+            for (int i = 0; i<10; i++) {
+                moviesList.add(movies.getBody().get(i));
             }
         }
-        model.addAttribute("movies", returnMovies);
+        model.addAttribute("movies", moviesList);
         return "movies/movies";
     }
 
-    @GetMapping("/search/{id}")
+    @GetMapping("/{id}")
     public String getMovieDetails(@PathVariable String id, Model model) {
         MovieDoc movie = webClient
                 .get()
